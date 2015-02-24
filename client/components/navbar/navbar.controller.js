c3a36396d4df687ee3ed38e6124d539c029eadca
Clanshood <clanshood@gmail.com>
2015-02-24 20:23:45 +0700

    create basic layout

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -3,7 +3,7 @@
 angular.module('smartPaperApp')
   .controller('NavbarCtrl', function ($scope, $location, Auth) {
     $scope.menu = [{
-      'title': 'Home',
+      'title': 'Smart Admin',
       'link': '/'
     }];
 
