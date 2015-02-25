e316a5fa3cff287eae67116c5989883347708ddf
Clanshood <clanshood@gmail.com>
2015-02-25 23:10:39 +0700

    create dinamic navbar template

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -2,15 +2,40 @@
 
 angular.module('smartPaperApp')
   .controller('NavbarCtrl', function ($scope, $location, Auth) {
-    $scope.menu = [{
-      'title': 'Smart Admin',
-      'link': '/'
-    }];
 
-    $scope.isCollapsed = true;
+    // required properties
+    $scope.isSidenavControll = true;
+    $scope.brand = "Smart Admin";
+    $scope.size = "";
+    // theming
+    $scope.theme = "default";
+
+
+    // optional properties
     $scope.isLoggedIn = Auth.isLoggedIn;
     $scope.isAdmin = Auth.isAdmin;
     $scope.getCurrentUser = Auth.getCurrentUser;
+    $scope.buttons = [{
+      label: 'Signup',
+      needAuth: false,
+      link: '/signup',
+      action: false
+    },{
+      label: 'Login',
+      needAuth: false,
+      link: '/login',
+      action: false
+    },{
+      label: 'Settings',
+      needAuth: true,
+      link: '/settings',
+      action: false
+    },{
+      label: 'Logout',
+      needAuth: true,
+      link: '/logout',
+      action: 'logout()'
+    }];
 
     $scope.logout = function() {
       Auth.logout();
