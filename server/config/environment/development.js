6fc14e97837a99745a10b4733cc94557b0344fc6
Clanshood <clanshood@gmail.com>
2015-03-19 19:33:26 +0700

    add paper seed for testing

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -8,5 +8,5 @@ module.exports = {
     uri: 'mongodb://localhost/smartpaper-dev'
   },
 
-  seedDB: false
+  seedDB: true
 };
--- server/config/seed.js
+++ server/config/seed.js
@@ -6,23 +6,52 @@
 'use strict';
 
 var User = require('../api/user/user.model');
+var Paper = require('../api/paper/paper.model');
 
-User.find({}).remove(function() {
-  User.create({
-    provider: 'local',
-    name: 'Test User',
-    username: 'test',
-    email: 'test@test.com',
-    password: 'test'
-  }, {
-    provider: 'local',
-    role: 'admin',
-    name: 'Admin',
-    username: 'admin',
-    email: 'admin@admin.com',
-    password: 'admin'
-  }, function() {
-      console.log('finished populating users');
+// User.find({}).remove(function() {
+//   User.create({
+//     provider: 'local',
+//     name: 'Test User',
+//     username: 'test',
+//     email: 'test@test.com',
+//     password: 'test'
+//   }, {
+//     provider: 'local',
+//     role: 'admin',
+//     name: 'Admin',
+//     username: 'admin',
+//     email: 'admin@admin.com',
+//     password: 'admin'
+//   }, function() {
+//       console.log('finished populating users');
+//     }
+//   );
+// });
+
+Paper.find({}).remove(function() {
+  Paper.create({
+    title: "Didn't need no welfare states",
+    description: "Everybody pulled his weight. Gee our old Lasalle ran great. Those were the days.",
+    questions: [{
+      fill: [
+        { question : "550943f5dea7f984072407df" },
+        { question : "550943f5dea7f984072407df" },
+        { question : "550943f5dea7f984072407df" },
+        { question : "550943f5dea7f984072407df" },
+        { question : "550943f5dea7f984072407df" },
+        { question : "550943f5dea7f984072407df" }
+      ]
+    }],
+    badge: ["XI IIA 2"],
+    label: ["ipa", "matematika"],
+    author: "550943f5dea7f984072407e0",
+    collaborators: [{
+      person: [{ author: "550943f5dea7f984072407e0"},
+        { author: "550943f5dea7f984072407df"}]
+    }]
+  },
+  function() {
+      console.log('finished populating paper');
     }
   );
 });
